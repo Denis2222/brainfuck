dc9361f72d4d4f849d01f6da7e42ed09472e2241
Denis2222 <denismoureu@gmail.com>
2016-02-09 01:58:06 +0100

    m

---
+++ brainfuck.c
@@ -0,0 +1,90 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+char *initmem(void)
+{
+	int	sizetab;
+	int	i;
+	char *mem;
+	
+	sizetab = 5000;
+	i = 0;
+	mem = (char*)malloc(sizeof(char) * sizetab + 1);
+	while (i < sizetab)
+	{
+		mem[i] = 0;
+		i++;
+	}
+	mem[i] = '\0';
+	return (mem);
+}
+
+void	brainfuck(char *code, char *mem)
+{
+	int 	c;
+	int 	i;
+	int		loop;
+	c = 0;
+	i = 0;
+	while (code[c])
+	{
+		if (code[c] == '>')
+			i++;
+		if (code[c] == '<')
+			i--;
+		if (code[c] == '.')
+			ft_putchar(mem[i]);
+		if (code[c] == '+')
+			mem[i]++;
+		if (code[c] == '-')
+			mem[i]--;
+		if (code[c] == ']')
+		{
+			loop = 1;
+			while (loop)
+			{
+				c--;
+				if (code[c] == ']')
+					loop++;
+				else if (code[c] == '[')
+					loop--;
+			}
+			c--;
+		}
+		if (code[c] == '[')
+		{
+			if (mem[i] == 0)
+			{
+				loop = 1;
+				while (loop)
+				{
+					c++;
+					if (code[c] == '[')
+						loop++;
+					else if (code[c] == ']')
+						loop--;
+				}
+			}
+		}
+		c++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	char	*code;
+	char	*mem;
+
+	if (ac == 2)
+	{
+		mem = initmem();
+		code = av[1];
+		brainfuck(code, mem);
+	}
+	return (0);
+}
